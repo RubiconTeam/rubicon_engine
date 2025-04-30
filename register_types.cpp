9678a6ede6ce519682261f8e08fc7738abd9ea6c
Binpuki <30415792+Binpuki@users.noreply.github.com>
2025-04-29 22:25:12 -0500

    Fix pathing issues, move core to own module

--- register_types.cpp
+++ register_types.cpp
@@ -1,33 +1,11 @@
 #include "register_types.h"
 
-#include "core/config/engine.h"
-#include "core/object/class_db.h"
-#include "rubicon_plus_plus/core/conductor.h"
-#include "rubicon_plus_plus/core/chart/time_change.h"
-
-static Conductor *ConductorPtr;
-
-void initialize_rubicon_plus_plus_module(ModuleInitializationLevel p_level) {
+void initialize_rubicon_module(ModuleInitializationLevel p_level) {
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
 		return;
-
-	// Register classes
-
-	// Rubicon.Core
-    GDREGISTER_CLASS(Conductor);
-
-	// Rubicon.Core.Chart
-	GDREGISTER_CLASS(TimeChange);
-
-	// Initialize singletons
-    ConductorPtr = memnew(Conductor);
-    Engine::get_singleton()->add_singleton(Engine::Singleton("Conductor", Conductor::get_singleton()));   
 }
 
-void uninitialize_rubicon_plus_plus_module(ModuleInitializationLevel p_level) {
+void uninitialize_rubicon_module(ModuleInitializationLevel p_level) {
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
 		return;
-
-    Engine::get_singleton()->remove_singleton("Conductor");
-	memdelete(ConductorPtr);
 }
--- register_types.h
+++ register_types.h
@@ -1,9 +1,9 @@
-#ifndef RUBICON_PLUS_PLUS_REGISTER_TYPES_H
-#define RUBICON_PLUS_PLUS_REGISTER_TYPES_H
+#ifndef RUBICON_REGISTER_TYPES_H
+#define RUBICON_REGISTER_TYPES_H
 
 #include "modules/register_module_types.h"
 
-void initialize_rubicon_plus_plus_module(ModuleInitializationLevel level);
-void uninitialize_rubicon_plus_plus_module(ModuleInitializationLevel level);
+void initialize_rubicon_module(ModuleInitializationLevel level);
+void uninitialize_rubicon_module(ModuleInitializationLevel level);
 
 #endif
