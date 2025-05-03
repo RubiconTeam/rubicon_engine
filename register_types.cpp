f12577b8fdaa181a3c88dbddd972cec6e4ea8f96
legole0 <legole0@proton.me>
2025-05-03 06:48:23 -0300

    Register types

--- register_types.cpp
+++ register_types.cpp
@@ -3,6 +3,8 @@
 #include "modules/rubicon/beat_syncer.h"
 #include "modules/rubicon/data/rubicon_dancer_data.h"
 #include "modules/rubicon/environment/rubicon_dancer_controller.h"
+#include "modules/rubicon/environment/rubicon_dancer_2d.h"
+#include "modules/rubicon/environment/rubicon_dancer_3d.h"
 
 void initialize_rubicon_module(ModuleInitializationLevel p_level) {
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
@@ -12,11 +14,13 @@ void initialize_rubicon_module(ModuleInitializationLevel p_level) {
 	GDREGISTER_CLASS(BeatSyncer);
 
 	// Rubicon.Environment
-	GDREGISTER_CLASS(RubiconDancerController)
+	GDREGISTER_CLASS(RubiconDancerController);
+	GDREGISTER_CLASS(RubiconDancer2D);
+	GDREGISTER_CLASS(RubiconDancer3D);
 
 	// Rubicon.Data
 	GDREGISTER_CLASS(BeatSyncerData);
-	GDREGISTER_CLASS(RubiconDancerData)
+	GDREGISTER_CLASS(RubiconDancerData);
 }
 
 void uninitialize_rubicon_module(ModuleInitializationLevel p_level) {
