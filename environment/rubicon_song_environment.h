45c2719b3e4a1ff9cad44a79826c30fb1d9da4d4
legole0 <legole0@proton.me>
2025-06-21 04:03:07 -0300

    Engine: Create RubiconSongEnvironment

---
+++ environment/rubicon_song_environment.h
@@ -0,0 +1,11 @@
+#ifndef RUBICON_SONG_ENVIRONMENT_H
+#define RUBICON_SONG_ENVIRONMENT_H
+
+#include "modules/rubicon_core/environment/rubicon_song.h"
+
+class RubiconSongEnvironment : public RubiconSong {
+    GDCLASS(RubiconSongEnvironment, RubiconSong);
+    
+};
+
+#endif
